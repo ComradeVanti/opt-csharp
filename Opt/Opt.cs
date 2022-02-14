08051a43b49d73b6ad9e4c7a0635e56afe43038d
ComradeVanti <kingvanti650@gmail.com>
2022-02-14 16:38:30 +0100

    Document opt creation logic

--- Opt/Opt.cs
+++ Opt/Opt.cs
@@ -32,20 +32,46 @@
     public static class Ext
     {
 
+        /// <summary>
+        ///     Checks if the optional is present
+        /// </summary>
+        /// <param name="opt">The optional</param>
+        /// <typeparam name="TValue">The type of the contained value</typeparam>
+        /// <returns>Whether a value is present or not</returns>
         public static bool IsSome<TValue>(this Opt<TValue> opt) =>
             opt is Some<TValue>;
 
+        /// <summary>
+        ///     Checks if the optional is missing
+        /// </summary>
+        /// <param name="opt">The optional</param>
+        /// <typeparam name="TValue">The type of the contained value</typeparam>
+        /// <returns>Whether a value is missing or not</returns>
         public static bool IsNone<TValue>(this Opt<TValue> opt) =>
             opt is None<TValue>;
 
     }
 
+    /// <summary>
+    ///     Contains convenience-methods for creating optionals
+    /// </summary>
     public static class Opt
     {
 
+        /// <summary>
+        ///     Creates a present optional
+        /// </summary>
+        /// <param name="value">The value to be contained in the optional</param>
+        /// <typeparam name="TValue">The type of the contained value</typeparam>
+        /// <returns>The created optional</returns>
         public static Opt<TValue> Some<TValue>(TValue value) =>
             new Some<TValue>(value);
 
+        /// <summary>
+        ///     Creates a missing optional
+        /// </summary>
+        /// <typeparam name="TValue">The type of the contained value</typeparam>
+        /// <returns>The created optional</returns>
         public static Opt<TValue> None<TValue>() =>
             new None<TValue>();
 
