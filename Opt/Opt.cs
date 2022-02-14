e4051634416b71621aed70a5ec674342712c2bb4
ComradeVanti <kingvanti650@gmail.com>
2022-02-14 16:27:26 +0100

    Add domain types

---
+++ Opt/Opt.cs
@@ -0,0 +1,31 @@
+﻿namespace ComradeVanti.CSharpTools
+{
+
+    /// <summary>
+    ///     A value that may be present or not
+    /// </summary>
+    /// <typeparam name="TValue">The type of the contained value</typeparam>
+    public abstract class Opt<TValue> { }
+
+    /// <summary>
+    ///     An optional value that is present
+    /// </summary>
+    /// <typeparam name="TValue">The type of the contained value</typeparam>
+    internal sealed class Some<TValue> : Opt<TValue>
+    {
+
+        public TValue Value { get; }
+
+
+        public Some(TValue value) =>
+            Value = value;
+
+    }
+
+    /// <summary>
+    ///     An optional value that is not present
+    /// </summary>
+    /// <typeparam name="TValue">The type of the contained value</typeparam>
+    internal sealed class None<TValue> : Opt<TValue> { }
+
+}
