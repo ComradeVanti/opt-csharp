880894785b252fbe3aefd9043d7e020d03f57c3b
ComradeVanti <kingvanti650@gmail.com>
2022-02-14 16:35:54 +0100

    Add logic to instantiate opts

---
+++ Opt.Tests/OptTests.cs
@@ -0,0 +1,28 @@
+﻿using FsCheck.Xunit;
+using Xunit;
+
+namespace ComradeVanti.CSharpTools
+{
+
+    public class OptTests
+    {
+
+        [Property]
+        public bool OptsCreatedWithSomeAreAlwaysSome(int i) =>
+            Opt.Some(i).IsSome();
+
+        [Property]
+        public bool OptsCreatedWithSomeAreNeverNone(int i) =>
+            !Opt.Some(i).IsNone();
+
+        [Fact]
+        public void OptsCreatedWithNoneAreAlwaysNone() =>
+            Assert.True(Opt.None<int>().IsNone());
+
+        [Fact]
+        public void OptsCreatedWithNoneAreNeverSome() =>
+            Assert.False(Opt.None<int>().IsSome());
+
+    }
+
+}
--- Opt/Opt.cs
+++ Opt/Opt.cs
@@ -28,4 +28,27 @@
     /// <typeparam name="TValue">The type of the contained value</typeparam>
     internal sealed class None<TValue> : Opt<TValue> { }
 
+
+    public static class Ext
+    {
+
+        public static bool IsSome<TValue>(this Opt<TValue> opt) =>
+            opt is Some<TValue>;
+
+        public static bool IsNone<TValue>(this Opt<TValue> opt) =>
+            opt is None<TValue>;
+
+    }
+
+    public static class Opt
+    {
+
+        public static Opt<TValue> Some<TValue>(TValue value) =>
+            new Some<TValue>(value);
+
+        public static Opt<TValue> None<TValue>() =>
+            new None<TValue>();
+
+    }
+
 }
