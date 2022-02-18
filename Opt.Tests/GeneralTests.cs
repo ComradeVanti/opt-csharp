b845c1d047d686f84da77ec4d813e9961e53ce12
ComradeVanti <kingvanti650@gmail.com>
2022-02-18 09:48:49 +0100

    Add test to check that some is never none

---
+++ Opt.Tests/GeneralTests.cs
@@ -0,0 +1,15 @@
+﻿using FsCheck.Xunit;
+
+namespace ComradeVanti.CSharpTools
+{
+
+    public class GeneralTests
+    {
+
+        [Property]
+        public bool OptsThatAreSomeAreNeverNone(Opt<int> opt) => 
+            opt.IsSome() != opt.IsNone();
+
+    }
+
+}
