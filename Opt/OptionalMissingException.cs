36c3f2d39ee3d590b57ecc6573527bea1919a9da
ComradeVanti <kingvanti650@gmail.com>
2022-02-20 12:13:25 +0100

    Reformat files

--- Opt/OptionalMissingException.cs
+++ Opt/OptionalMissingException.cs
@@ -2,10 +2,10 @@
 
 namespace ComradeVanti.CSharpTools
 {
-    
+
     /// <summary>
-    /// Exception thrown when a missing optional is accessed. Equivalent to a
-    /// conventional null-pointer exception.
+    ///     Exception thrown when a missing optional is accessed. Equivalent to a
+    ///     conventional null-pointer exception.
     /// </summary>
     public class OptionalMissingException : InvalidOperationException
     {
