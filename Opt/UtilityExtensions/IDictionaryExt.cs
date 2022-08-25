4418b0e7a14efb6d6acf0e4294c169dacf60c028
ComradeVanti <kingvanti650@gmail.com>
2022-08-25 08:56:55 +0200

    Document extension methods

--- Opt/UtilityExtensions/IDictionaryExt.cs
+++ Opt/UtilityExtensions/IDictionaryExt.cs
@@ -7,6 +7,14 @@ namespace ComradeVanti.CSharpTools.UtilityExtensions
     public static class IDictionaryExt
     {
 
+        /// <summary>
+        ///     Attempts to get a value from the dictionary
+        /// </summary>
+        /// <param name="dict">The dictionary</param>
+        /// <param name="key">The key</param>
+        /// <typeparam name="TKey">The type of the key</typeparam>
+        /// <typeparam name="TValue">The type of the value</typeparam>
+        /// <returns>The value or none if not found</returns>
         public static IOpt<TValue> TryGet<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key) =>
             dict.TryGetValue(key, out var value)
                 ? Opt.Some(value)
--- Opt/UtilityExtensions/IEnumerableExt.cs
+++ Opt/UtilityExtensions/IEnumerableExt.cs
@@ -8,16 +8,44 @@ namespace ComradeVanti.CSharpTools.UtilityExtensions
     public static class IEnumerableExt
     {
 
+        /// <summary>
+        ///     Attempts to get the first element from the sequence
+        /// </summary>
+        /// <param name="items">The sequence</param>
+        /// <typeparam name="T">The type of item in the sequence</typeparam>
+        /// <returns>The first item or None, if the sequence is empty</returns>
         public static IOpt<T> TryFirst<T>(this IEnumerable<T> items) =>
             Opt.FromOp(items.First);
 
+        /// <summary>
+        ///     Attempts to get the last element from the sequence
+        /// </summary>
+        /// <param name="items">The sequence</param>
+        /// <typeparam name="T">The type of item in the sequence</typeparam>
+        /// <returns>The last item or None, if the sequence is empty</returns>
         public static IOpt<T> TryLast<T>(this IEnumerable<T> items) =>
             Opt.FromOp(items.Last);
 
+        /// <summary>
+        ///     Attempts to get an element from the sequence by its index
+        /// </summary>
+        /// <param name="items">The sequence</param>
+        /// <param name="index">The index of the item to get</param>
+        /// <typeparam name="T">The type of item in the sequence</typeparam>
+        /// <returns>The item or None, if no such index exists</returns>
         public static IOpt<T> TryElementAt<T>(this IEnumerable<T> items, int index) =>
             Opt.FromOp(() => items.ElementAt(index));
 
-        public static IOpt<IEnumerable<T>> Collect<T>(this IEnumerable<IOpt<T>> opts) => 
+        /// <summary>
+        ///     Collects a sequence of optionals
+        /// </summary>
+        /// <param name="opts">The sequence</param>
+        /// <typeparam name="T">The type of the contained value</typeparam>
+        /// <returns>
+        ///     The sequence if all optionals were present or none if one or more
+        ///     where missing
+        /// </returns>
+        public static IOpt<IEnumerable<T>> Collect<T>(this IEnumerable<IOpt<T>> opts) =>
             Opt.FromOp(() => opts.Select(it => it.Get()));
 
     }
