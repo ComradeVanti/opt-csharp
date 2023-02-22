a35f92189f61765a36e3c039d1acabbfb938b7e1
ComradeVanti <kingvanti650@gmail.com>
2023-02-22 11:24:08 +0100

    feat: add test for try-single function

---
+++ Opt.Tests/IEnumerableExtTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace ComradeVanti.CSharpTools;
+
+// ReSharper disable once InconsistentNaming
+public class IEnumerableExtTest
+{
+    [Property]
+    public bool TrySingleIsSomeForSingletonCollections(int item)
+    {
+        var collection = new[] {item};
+        var opt = collection.TrySingle();
+        return opt.IsSome();
+    }
+
+    [Fact]
+    public void TrySingleIsNoneForEmptyCollections()
+    {
+        var collection = Array.Empty<int>();
+        var opt = collection.TrySingle();
+        Assert.True(opt.IsNone());
+    }
+
+    [Fact]
+    public void TrySingleIsNoneForCollectionsWithMoreThanOneItem()
+    {
+        var collection = new[] {1, 2, 3};
+        var opt = collection.TrySingle();
+        Assert.True(opt.IsNone());
+    }
+}
